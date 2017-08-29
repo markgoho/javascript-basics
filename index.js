b970da21395df09377c4ae9b9de14c3d2337c441
markgoho <markgoho@gmail.com>
2017-08-28 21:30:32 -0400

    conditional statements

---
+++ index.js
@@ -0,0 +1,27 @@
+let number = 0;
+
+// while (number <= 12) {
+//   console.log(number);
+//   number = number + 2;
+// }
+
+// for (let i = 0; i < 10; i += 2) {
+//   console.log(i);
+// }
+
+let debt = 130;
+
+switch (debt) {
+  case 50:
+    console.log('You need to pay some money.');
+    break;
+  case 40:
+    console.log('You should pay some money.');
+    break;
+  case 30:
+    console.log('You might want to pay some money');
+    break;
+  default:
+    console.log('Who knows how much money you owe?');
+    break;
+}
