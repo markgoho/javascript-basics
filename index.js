acc8c41c729f185fc7b47a81f1613c5ed289a3eb
Mark Goho <markgoho@gmail.com>
2017-09-09 17:12:24 -0400

    Adding some basic DOM manipulation

--- index.js
+++ index.js
@@ -1,63 +1,31 @@
-const num = 37.5;
-const str = 'Hello world!';
-const bool = true;
-const first = 3;
-const second = 5;
-const arr = [num, str, bool, 'Another string', 5324, true, false];
-
-//console.log(arr[0]);
-
-///////////////////
-// Array Methods //
-///////////////////
-
-// Add, remove elements
-arr.push('Haha!'); // adds 'haha' into the array as the last element
-arr.pop(); // removes the element at the last index
-arr.unshift('Haha!'); // adds 'Haha!' into the array, first element
-arr.shift(); // removes first element
-
-arr.indexOf('Hello world!'); // returns 1 because that string is at index 1
-
-// Slice
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-//console.log(alphabet.slice(1, 3)); // returns a new array, start to end
-
-// Join
-const newArr = ['more', 'items', true, 53];
-arr.join(''); // returns a string, with , separator
-
-////////////////////
-// String Methods //
-////////////////////
-const newString = `  Don't have a cow man!  `;
-
-newString.length; // -> 21
-newString.trim(); // removes whitespace
-newString.toUpperCase(); // makes all characters uppercase
-
-/////////////////
-// Math Object //
-/////////////////
-// console.log(Math.ceil(14.5)); // goes to the nearest whole number below what's given
-// console.log(Math.round(10.5));
-
-// console.log(Math.PI);
-
-// console.log(Math.floor(Math.random() * 10));
-
-/////////////
-// Objects //
-/////////////
-
-const francisco = {
-  'height in feet': 6,
-  weight: 195,
-  name: 'Francisco',
-  goodAtSoccer: true,
-  cook: function() {
-    console.log('I made a meal');
+const questions = [
+  {
+    question: 'How many states are in the United States?',
+    answer: 50
+  },
+  {
+    question: 'How many continents are there?',
+    answer: 7
+  },
+  {
+    question: 'How many legs does an insect have?',
+    answer: 6
   }
-};
+];
 
-francisco.cook();
+const summaryEl = document.querySelector('.summary');
+
+let correct = 0;
+
+const print = message => {};
+
+for (const q of questions) {
+  const response = parseInt(prompt(q.question));
+  if (response === q.answer) {
+    correct++;
+  }
+}
+
+summaryEl.innerHTML = `You got ${correct} correct. That is a ${Math.round(
+  correct / questions.length * 100
+)}%`;
